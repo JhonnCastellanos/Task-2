1f35d831d279f6fe66f99251ca8deb4f477dc9e1
Jhonn Castellanos <jhonn.castellanos@globant.com>
2022-08-08 11:41:08 -0500

    Add Delete and change README note

--- Cypress/e2e/testcases/Pet/Delete/Pet-Delete-petId.cy.js
+++ Cypress/e2e/testcases/Pet/Delete/Pet-Delete-petId.cy.js
@@ -1,30 +1,15 @@
 /// <reference types="cypress" />
-describe('PUT Update an existing pet', function () {
-    it('Sending PUT to Pet', function () {
-      var response = cy.request({
-        method: 'PUT',
-        url: 'http://localhost:8080/api/v3/pet/',
-        body: {
-            "id": 10,
-            "name": "doggie",
-            "category": {
-              "id": 1,
-              "name": "Dogs"
-            },
-            "photoUrls": [
-              "string"
-            ],
-            "tags": [
-              {
-                "id": 0,
-                "name": "string"
-              }
-            ],
-            "status": "available"
-          }
-      }).then((response) => {
-        expect(response).property('status').to.equal(200)
-        cy.log(response.body)
-      })
+describe('DELETE delete a pet', function () {
+  it('Sending DELETE to Pet', function () {
+    var response = cy.request({
+      method: 'DELETE',
+      url: 'http://localhost:8080/api/v3/pet/100',
+      body:
+      {
+      }
+    }).then((response) => {
+      expect(response).property('status').to.equal(200)
+      cy.log(response.body)
     })
   })
+})
--- README.md
+++ README.md
@@ -1,51 +1,9 @@
 # test-automation
-Any test automation code for all pvolve services
+Any test automation code personal proporse
 
 ### Install
 ```
-git clone https://github.com/p-volve/test-automation.git
-cd test-automation/web-cypress
+git clone https://github.com/JhonnCastellanos/Task-2.git
+cd cypress
 npm install
 ```
-
-### Usage
-Run all tests(headless) with Allure reports enabled
-```
-npm run cy:qa:runall
-```
-```
-npm run cy:qa2:runall
-```
-```
-npm run cy:dev:runall
-```
-<br></br>
-Open Cypress Test Runner with Allure reports enabled
-```
-npm run cy:qa:open
-```
-```
-npm run cy:qa2:open
-```
-```
-npm run cy:dev:open
-```
-<br></br>
-Open Cypress Test Runner without Allure reports
-```
-npx cypress open --env configFile=qa
-```
-```
-npx cypress open --env configFile=qa2
-```
-```
-npx cypress open --env configFile=dev
-```
-```
-npx cypress open //This will default to qa environment
-```
-<br></br>
-<br></br>
-### API-Tests
-(These tests are currently not maintained)(related to python language)\
-Run inside command line to run the tests: python3 -m pytest tests --environment=qa 
